9c634ef205e79ac4980392b9ec1346f8fd0172d2
Harish Tagadghar <harishtgtg@gmail.com>
2020-06-20 23:50:48 +0530

    adding title and discription from array

---
+++ src/Notes.js
@@ -0,0 +1,14 @@
+let Notes = [
+    {
+        key : 1,
+        title:"react",
+        content:"javascript framework user for fontend"
+    },
+    {
+        key:2,
+        title:"shopify",
+        content:"shopify dropshipping which ship item from aliexpress"
+    }
+]
+
+export default Notes ;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import Notes from "../Notes";
 
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+           { Notes.map((note) =>
+           <Note 
+               key={note.key}
+               title={note.title}
+               content={note.content}
+           />
+           )}
             <Footer />
         </div>
     )
